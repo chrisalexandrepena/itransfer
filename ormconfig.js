67314261cfa7ff9fc8aa5f8d3d77f9bc1e68722d
Chris-Alexandre Pena <chrisalexandre.pena@gmail.com>
2020-04-13 19:14:03 +0200

    added ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '0', 0) || 5432,
+  username: process.env.DB_USER || 'user',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'custom_wetransfer',
+  entities: ['src/db/entities/*.ts'],
+  migrations: ['src/db/migrations/*.ts'],
+  cli: {
+    entitiesDir: 'src/db/entities',
+    migrationsDir: 'src/db/migrations',
+  },
+};
