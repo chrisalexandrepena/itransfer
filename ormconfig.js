4d2ee02fbe56035e578f5769ae0b86b807d48f8d
Chris-Alexandre Pena <chrisalexandre.pena@gmail.com>
2020-04-14 20:01:49 +0200

    :lipstick: changed default value for database in ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -7,7 +7,7 @@ module.exports = {
   port: parseInt(process.env.POSTGRES_PORT || '0', 0) || 5432,
   username: process.env.POSTGRES_USER || 'user',
   password: process.env.POSTGRES_PASSWORD || 'password',
-  database: process.env.POSTGRES_DB || 'custom_wetransfer',
+  database: process.env.POSTGRES_DB || 'itransfer',
   entities: ['lib/db/entities/*.js'],
   migrations: ['lib/db/migrations/*.js'],
   cli: {
